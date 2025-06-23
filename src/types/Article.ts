144264278e5e12f8a19436784c912449c15547a9
Matheus Vieira <matheus.msv1997@gmail.com>
2025-06-23 20:08:52 -0300

    refactor: move Article interface to a separate type file and update API URL fetching to use env variable

---
+++ src/types/Article.ts
@@ -0,0 +1,11 @@
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  tag: string;
+  image_url: string;
+  published_at: string;
+  created_at: string;
+  updated_at: string;
+}
