2c52d7ae45871934eab3167508164ab72c6d1e1d
Matheus Vieira <matheus.msv1997@gmail.com>
2025-06-23 22:30:59 -0300

    feat: add ArticlesList component to display a list of articles with ArticleCard

---
+++ src/components/ArticlesList.tsx
@@ -0,0 +1,26 @@
+import { Article } from '@/types/Article';
+import ArticleCard from '@/components/ArticleCard';
+
+interface ArticlesListProps {
+  articles: Article[];
+}
+
+export default function ArticlesList({ articles }: ArticlesListProps) {
+  return (
+    <section
+      className="flex flex-row flex-wrap gap-4 sm:gap-6 justify-center mt-4 lg:mx-50"
+      aria-label="All Articles"
+    >
+      {articles.map((article, index) => (
+        <ArticleCard
+          key={article.id || index}
+          tag={article.tag}
+          title={article.title}
+          author={article.author}
+          date={article.published_at}
+          imageUrl={article.image_url}
+        />
+      ))}
+    </section>
+  );
+}
